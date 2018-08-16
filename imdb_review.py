be4bfc2114950917bc63ad9b6185c25edd92ff20
Avneet <36086810+avnbad@users.noreply.github.com>
2018-08-16 10:06:41 +0530

    Add files via upload

---
+++ imdb_review.py
@@ -0,0 +1,81 @@
+# Step 1: 
+#load the data set
+import numpy
+from keras.datasets import imdb
+from matplotlib import pyplot
+
+
+# load the dataset
+(X_train, y_train), (X_test, y_test) = imdb.load_data()
+X = numpy.concatenate((X_train, X_test), axis=0)
+y = numpy.concatenate((y_train, y_test), axis=0)
+
+# summarize size
+print("Training data: ")
+print(X.shape)
+print(y.shape)
+
+# Summarize number of classes
+print("Classes: ")
+print(numpy.unique(y))
+
+# Summarize number of words
+print("Number of words: ")
+print(len(numpy.unique(numpy.hstack(X))))
+
+
+# Summarize review length
+print("Review length: ")
+result = [len(x) for x in X]
+print("Mean %.2f words (%f)" % (numpy.mean(result), numpy.std(result)))
+
+# plot review length
+pyplot.boxplot(result)
+pyplot.show()
+
+# Step 2:
+#Embedding is also included 
+
+# MLP for the IMDB problem
+import numpy
+from keras.datasets import imdb
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
+# fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
+
+
+# load the dataset but only keep the top n words, zero the rest
+#Embedding
+top_words = 5000
+(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=top_words)
+
+
+
+max_words = 500
+X_train = sequence.pad_sequences(X_train, maxlen=max_words)
+X_test = sequence.pad_sequences(X_test, maxlen=max_words)
+
+
+#step 3:
+
+# create the model
+model = Sequential()
+model.add(Embedding(top_words, 32, input_length=max_words))
+model.add(Flatten())
+model.add(Dense(250, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model.summary())
+
+
+# Fit the model
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=2, batch_size=128, verbose=2)
+
+# Final evaluation of the model
+scores = model.evaluate(X_test, y_test, verbose=0) 
+print("Accuracy: %.2f%%" % (scores[1]*100))
